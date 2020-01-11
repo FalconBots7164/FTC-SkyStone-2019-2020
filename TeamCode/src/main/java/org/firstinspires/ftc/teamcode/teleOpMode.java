94dd598bda7d7e26603fdac3df99807e6825aa03
FalconBots7164 <locustvalleyfalconbots@gmail.com>
2020-01-11 13:08:13 -0500

    Merge branch 'master' of https://github.com/FalconBots7164/FTC-SkyStone-2019-2020
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpModeDAM.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpMode.java
@@ -103,8 +103,8 @@ public class teleOpMode extends OpMode {
         }
 
         //gamepad2
-        slide.setPower(gamepad2.right_stick_y * powerB * .3);
-        lift.setPower(gamepad2.left_stick_y * powerB * .65);
+        slide.setPower(gamepad2.left_stick_y * powerB * .5);
+        lift.setPower(gamepad2.right_stick_y * powerB * .65);
 
         if(gamepad2.a && !isToggledB && !lockB) {
             powerB = .25;
